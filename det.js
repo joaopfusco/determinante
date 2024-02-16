82b05d438164c2a136392c88f1e3618012bdaf4b
joaopfusco <125307931+joaopfusco@users.noreply.github.com>
2024-02-16 19:51:05 -0300

    Update det.js

--- det.js
+++ det.js
@@ -69,7 +69,7 @@ function calcular() {
             matriz[linhaTriangular] = matriz[linhaTriangular].map((el, index) => el + k * matriz[linha][index]);
         }
 
-        const det = prodDiagonalPrincipal(matriz);
+        const det = round(prodDiagonalPrincipal(matriz), 2);
         console.log(matriz)
         console.log(det);
         window.alert(det);
